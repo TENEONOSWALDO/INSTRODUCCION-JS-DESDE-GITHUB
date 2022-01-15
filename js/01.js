8a3c8072d9e53f7aed1a1198c519c2b8f65a3321
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-14 21:19:29 -0500

    DEfiniendo variables

--- js/01.js
+++ js/01.js
@@ -1,2 +1,13 @@
 //variables
-var producto = 'Audifonos Gamer'
+var producto = 'Audifonos Gamer'; //nIniciar variable y signarle valor
+var disponible; // Iniciamos la variable pero sin valor.
+producto = true;// reasignando el valor de la variable.
+disponible = true;// reasignando el valor de la variable
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+var disponible;// Las varibles no pueDen iniciar con numeros ni - pero si finalizar
+var producto1;// Puede finalizar con numero
+var _producto; //Si esta disponible//
