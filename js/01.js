be3fac2abc3f0783e6a1f46d2f682204d5cef4d2
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-17 14:12:01 -0500

    DESCRIPCION DE VARIABLES

--- js/01.js
+++ js/01.js
@@ -1,13 +1,25 @@
 //variables
 var producto = 'Audifonos Gamer'; //nIniciar variable y signarle valor
 var disponible; // Iniciamos la variable pero sin valor.
-producto = true;// reasignando el valor de la variable.
+// producto = true;// reasignando el valor de la variable.
 disponible = true;// reasignando el valor de la variable
 
 var producto1 = 'Computadora',
     disponible1 = true,
     categoria = 'Computadoras';
 
-var disponible;// Las varibles no pueDen iniciar con numeros ni - pero si finalizar
+//var 1disponible;// Las varibles no pueDen iniciar con numeros ni - pero si finalizar
 var producto1;// Puede finalizar con numero
 var _producto; //Si esta disponible//
+//var produ-cto;// No esta Permitido
+var produc_to;//Esta  permitido
+var producto_;//Si esta Permitido
+
+//Estilos para las variables
+
+var nombre_proDucto = 'Monitor HD';
+var nombreProrodducto = 'Monitor HD';
+var NombreProducto = 'Monitor HD';
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
