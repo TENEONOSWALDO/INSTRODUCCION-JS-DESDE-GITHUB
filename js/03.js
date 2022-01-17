480a06c0f76e4eaafe03b5b47feba470a6902d73
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-17 14:50:10 -0500

    se creo el archivo 03.js  y variables conts

---
+++ js/03.js
@@ -0,0 +1,22 @@
+// Variables con const//
+//variables
+const producto = 'Audifonos Gamer'; //nIniciar variable y signarle valor
+const disponible; // Iniciamos la variable pero sin valor.
+// producto = true;// reasignando el valor de la variable.
+disponible = true;// reasignando el valor de la variable
+
+const producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//let 1disponible;// Las varibles no pueDen iniciar con numeros ni - pero si finalizar
+const producto1;// Puede finalizar con numero
+const _producto; //Si esta disponible//
+//let produ-cto;// No esta Permitido
+const produc_to;//Esta  permitido
+const producto_;//Si esta Permitido
+
+//Estilos para las variablesconst nombre_proDucto = 'Monitor HD';
+const nombreProrodducto = 'Monitor HD';
+const NombreProducto = 'Monitor HD';
+const nombreproducto = 'Monitor HD';
