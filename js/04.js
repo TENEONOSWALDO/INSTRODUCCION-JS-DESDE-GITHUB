73b798798b9192a1cbe67320c49fd0359ecb12db
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-20 12:01:56 -0500

    se introdujeron cambios en las variables

---
+++ js/04.js
@@ -0,0 +1,18 @@
+// String o cadena de texto
+//const producto = "Monitor de 20 Pulgadas";
+//const producto1 = "Monitor de 20\"";
+//const producto2 = String("Monitor de 30 Pulgadas");
+//const producto3 = new String("Monitor de 50 Pulgadas");//new agrega características a la información.
+//onsole.log(typeof producto);//typeof indica que tipo de variable es.
+//console.log(producto2);
+//console.log(producto1);
+//console.log(producto3);
+
+const producto = 'Monitor de 20"';
+const producto2 = 'Monitor HD';
+
+console.log(producto.length)
+// Indexof
+
+const tweet = 'Aprendiendo javaScript con el curso de Desarrollo Web Completo';
+console.log(tweet.indexOf('javaScript')) ;
