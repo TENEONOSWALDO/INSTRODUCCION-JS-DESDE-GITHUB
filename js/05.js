3d9c6441a2648b4378ab1d431d3ffda159f17ff0
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-20 13:49:54 -0500

    se explican operaciones matematicas

---
+++ js/05.js
@@ -0,0 +1,22 @@
+//Numeros 
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = "300";
+const numero4 = 200.20;
+const numero5 = .1020;
+const numero6 = -5;
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);//Este numero aparece en color negro en la consola, es un strint
+console.log(numero4);
+console.log(numero5);
+console.log(numero6);
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero2 * numero1);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1);
+
+//Para explicar lo que es el modulo ( % ) hagamos lo siguiente
