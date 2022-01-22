eb25a58e480fb32876eb28d555cc08f22bc4f8b2
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-21 21:36:48 -0500

    incremento de 1 en 1 y decremento

--- js/07.js
+++ js/07.js
@@ -3,5 +3,26 @@
 let resultado;
 
 resultado= 20 + 30 * 2;
+resultado= (20 + 30) * 2;
+resultado = ( 100 + 200 + 300 ) *.2 // 20% 
 
-console.log(resultado);
+//console.log(resultado);
+
+// Incrementos
+
+let puntaje =  10;
+//incremento en 1
+puntaje++;
+
+console.log(puntaje++);
+console.log(++puntaje);
+console.log(puntaje++);
+console.log(puntaje++);
+puntaje--;
+
+console.log(puntaje--);
+console.log(--puntaje);
+
+puntaje += 10;
+
+console.log(puntaje);
