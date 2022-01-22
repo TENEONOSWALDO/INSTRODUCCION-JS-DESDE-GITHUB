a5f32c06c5bd6be60180e8f501024d20d93a894c
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-22 18:23:35 -0500

    generacion fle 08.js y ejemplos de cocatenacion y Template string

---
+++ js/08.js
@@ -0,0 +1,12 @@
+//Concatenacion
+
+const nombre = 'juan';
+const email = 'correo@correo.com';
+console.log(nombre + ' ' + email);
+console.log(email + ' ' + nombre);
+
+console.log("Nombre Cliente:", nombre , "       " ,"Email:",email);
+
+//Template string- String Literals
+console.log(`Nombre Cliente: ${nombre}         Email: ${email}`);
+
