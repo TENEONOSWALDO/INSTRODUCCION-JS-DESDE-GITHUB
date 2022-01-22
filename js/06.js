05a2f3277a872e24183d9fb8595a1c698e190588
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-21 20:56:56 -0500

    crear 07.js

---
+++ js/06.js
@@ -0,0 +1,16 @@
+//Objeto Math
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.2);
+resultado = Math.round(2.2);//Redondea hacia arriba.
+resultado = Math.floor(2.9);//Redondea hacia abajo.
+resultado = Math.sqrt(144);//Raiz cuadrada.
+resultado = Math.abs(-200);//Valor absoluto.
+resultado = Math.min( 3, 5, 1, 8, 2, 10);//Minimo
+resultado = Math.max( 3, 5, 1, 8, 2, 10);//Maximo
+resultado = Math.random();
+resultado = Math.floor( Math.random() * 30 );
+
+
+console.log(resultado);
