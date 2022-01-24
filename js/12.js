747c4506de50f968819cf900cb403be0bad0663c
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-24 17:39:32 -0500

    como unir dos objetos con Spread Operator

--- js/12.js
+++ js/12.js
@@ -1,11 +1,17 @@
-"use strict";//Correr JS en modo estricto
+"use strict";//Correr JS en modo estrict
+//Objetos
 const producto = {
     nombreProducto : "Monitor 20 Pulgadas",
     precio : 300,
     disponible : true
     }
 
-Object.freeze(producto); //Frezze no permiter agreagarle nuevas propiedades, ni que el objeto
+Object.seal(producto); //Frezze no permiter agreagarle nuevas propiedades, ni que el objeto
    // producto.imagen = 'imagen.jpg';
+producto.precio = 'NUEVO PRECIO' ;
+
 console.log(Object.isFrozen(producto)) ;
     console.log(producto) ;
+
+ delete producto.precio ;   
+ console.log(producto) ;
