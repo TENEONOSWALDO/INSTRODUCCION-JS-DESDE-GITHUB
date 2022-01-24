b51f872abd022b565b3d4492b6ab7d5d9d5d2ef8
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-23 19:47:18 -0500

    Destructuring de un objeto

---
+++ js/11.js
@@ -0,0 +1,12 @@
+
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas",
+    precio : 300,
+    disponible : true
+    }
+
+    const precioProducto = producto.precio ;
+    const nombreProducto = producto.nombreProducto ;
+
+
+    console.log(nombreProducto);
