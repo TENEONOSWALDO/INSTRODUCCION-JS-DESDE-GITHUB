677d28bbe0482435e89dc6224f5f8d0d2b188d77
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-23 19:37:22 -0500

    Eliminar y agregar propiedades a los objetos

--- js/10.js
+++ js/10.js
@@ -1,8 +1,8 @@
 //Objetos
 
-const nombreProducto = "Monitor 20 Pulgadas"
-const precio = 300 ;
-const disponibe = true ;
+//const nombreProducto = "Monitor 20 Pulgadas"
+//const precio = 300 ;
+//const disponibe = true ;
 
 const producto = {
 nombreProducto : "Monitor 20 Pulgadas",
@@ -10,12 +10,20 @@ precio : 300,
 disponible : true
 }
 
-console.log(producto);
+//console.log(producto);
 
-console.log(producto.precio);
-console.log(producto.nombreProducto);
-console.log(producto.disponible);
+//console.log(producto.precio);
+//console.log(producto.nombreProducto);
+//console.log(producto.disponible);
 
-console.log(producto["precio"]);
-console.log(producto["nombreProducto"]);
-console.log(producto["disponible"]);
+//console.log(producto["precio"]);
+//console.log(producto["nombreProducto"]);
+//onsole.log(producto["disponible"]);
+
+
+//Agregar nuevas propiedades
+
+producto.imagen = 'imagen.jpg';
+//Eliminar propiedades
+delete producto.disponible;
+console.log (producto) ;
