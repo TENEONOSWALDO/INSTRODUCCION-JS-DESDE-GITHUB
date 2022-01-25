c57db19b0aafa56d4e90bddb4a6d960b46748fd2
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-24 19:36:04 -0500

    como acceder a los valores de un arreglo

---
+++ js/14.js
@@ -0,0 +1,19 @@
+//Arreglos o Arrays
+
+const numeros = [10,20,30,40,50 ];
+console.log(numeros);
+console.table(numeros);
+
+//const meses = new Array ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Jul');
+//console.table(meses);//Constructor
+
+const meses =  ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Jul'] ;
+console.table(meses);
+
+const arreglo =[ "Hola", 10, true, "si", null, {nombre: "Juan", trabajo: "Programador"}, [1,2,3]] ;
+console.table(arreglo);
+console.log(arreglo);
+
+// Acceder a los valores de un arreglo
+
+console.log(numeros[4]);
