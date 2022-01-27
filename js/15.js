7ce85352fcf3bcd3e4d98b8eba50e2c58df4c65a
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-26 20:46:57 -0500

    se explica include

--- js/15.js
+++ js/15.js
@@ -1,3 +1,5 @@
+
+// Array Methods
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
 
 const carrito = [
@@ -10,4 +12,20 @@ const carrito = [
 { nombre : 'Bocinas', precio: 300 },
 { nombre : 'Laptop', precio: 800 },
 ];
-//
+//  forEach
+meses.forEach(function(mes) { 
+    console.log(mes);
+});
+
+meses.forEach(function(mes) {  // EStructura de control
+    if ( mes=='Marzo') {
+        console.log ('Marzo si existe');
+    }
+});
+
+//includes
+const resultado = meses.includes('Marzo');
+console.log(resultado);
+
+//const resultado = meses.includes('Diciembre');
+//console.log(resultado);
