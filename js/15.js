0b17f4f1401ff9ec3786680e2e8ee7a6eadb0526
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-26 21:16:53 -0500

    como se utiliza reduce

--- js/15.js
+++ js/15.js
@@ -24,8 +24,25 @@ meses.forEach(function(mes) {  // EStructura de control
 });
 
 //includes
-const resultado = meses.includes('Marzo');
+//consoleconst resultado = meses.includes('Marzo');
+//console.log(resultado);
+
+let resultado = meses.includes('Diciembre');// Se emplea cuando es un arreglo plano
+const resultado2 = carrito.includes('Celular');
+console.log(resultado);
+console.log(resultado2);// Celular si existe  y pone false
+
+// Sonbe para arreglo de Objetos
+
+    resultado = carrito.some(function(producto){
+    return producto.nombre === 'Celular'
+})
 console.log(resultado);
 
-//const resultado = meses.includes('Diciembre');
-//console.log(resultado);
+//Reduce the resultado
+
+resultado = carrito.reduce(function(total, producto){
+    return total + producto.precio
+} , 0);
+console.log( resultado);
+
