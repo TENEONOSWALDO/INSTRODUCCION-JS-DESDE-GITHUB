e999ba1918326b6f29f8bf43dcd4bf1ffe599bc5
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-26 20:26:47 -0500

    generar un arreglo de objetos

--- js/15.js
+++ js/15.js
@@ -1,1 +1,13 @@
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+
+const carrito = [
+{ nombre : 'Monitor 20 Pulgadas', precio: 500 },
+{ nombre : 'Television 50 Pulgadas', precio: 700 },
+{ nombre : 'Tablet', precio: 300 },
+{ nombre : 'Audifonos', precio: 200 },
+{ nombre : 'Teclado', precio: 50 },
+{ nombre : 'Celular', precio:  500 },
+{ nombre : 'Bocinas', precio: 300 },
+{ nombre : 'Laptop', precio: 800 },
+];
+//
