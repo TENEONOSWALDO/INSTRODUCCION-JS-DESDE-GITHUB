7d114b4d50045848e9d4505914362bd1e2877efa
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-27 20:20:38 -0500

    Diferencia entre funcion y metodo

---
+++ js/17.js
@@ -0,0 +1,7 @@
+ const numero1 = 20 ;
+ const numero2 = "20" ;
+
+console.log(numero1);
+console.log (parseInt (numero2)) ;//  funcion parseInt convierte un string en un numero. Una funcion seguido del parentesis.
+console.log(numero1.toString());// .toString es un metodo. Un punto seguido de la funcion
+
