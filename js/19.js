f2c5f2c1dab5638625db8b9f7911d9a45e5241f1
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-29 17:25:11 -0500

    Mensaje de vtotal a pagar

--- js/19.js
+++ js/19.js
@@ -21,4 +21,4 @@ total= agregarCarrito(600);
 
 console.log(total);
 const totalApagar = calcularImpuesto(total);
-console.log('El total a pagar con impuestos es de: ${total Apagar}');
+console.log(`El total a pagar con impuesto es de: $${totalApagar}`);
