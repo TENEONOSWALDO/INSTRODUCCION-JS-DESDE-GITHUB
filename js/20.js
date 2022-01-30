d8566068f05f71552df354e301110448bdacd63a
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-01-29 21:32:33 -0500

    Crear file 21.js, activar 21.js

--- js/20.js
+++ js/20.js
@@ -2,8 +2,23 @@
 const reproductor = {
     reproducir : function(id){
         console.log(`Reproduciendo cancion con el id : ${id}`)
-    }
+    },
+    pausar: function(){
+        console.log ('Pausando...')
+    }, 
+    crearPlaylist: function(nombre) {
+        console.log(`Creando la playlist: ${nombre}`)
+    }, 
+    reproducirPlaylist: function(nombre) {
+        console.log(`Reproduciendo la playlist: ${nombre}`)
+    },
 }
-console.log(reproductor);
+reproductor.borrarCancion = function (id){
+    console.log(`Eliminando la cancion: ${id}`)
+}
 
 reproductor.reproducir(3840);
+reproductor.pausar();
+reproductor.borrarCancion(20);
+reproductor.crearPlaylist('Heavy Metal');
+reproductor.reproducirPlaylist('Heavy Metal');
---
+++ js/21.js
@@ -0,0 +1,9 @@
+//Arrows function
+
+const sumar2 = function (n1, n2){// n1 y n2 parametros
+    console.log( n1 + n2);
+}
+
+
+sumar2(5, 10);// 5 y 10 argumentos
+
