1918e91f588b30b42f2a6f8ebd571fd0c16b2f8f
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-01 20:11:06 -0500

    Switch mejor opcion que else if

--- js/23.js
+++ js/23.js
@@ -1,13 +1,18 @@
 
-const metodoPago = 'tarjeta';
+const metodoPago = 'efectivo';
 
 
 switch (metodoPago) {
     case 'tarjeta':
         console.log('Pagaste con tarjeta');
         break;
-
+    case 'cheque':
+        console.log('El usuario pagara con cheque, revisar fondos');
+        break;
+    case 'efectivo':
+        console.log('Pagaste con efectivo') ;
+        break;
     default:
-        console.log('Aun no has pagado')
+        console.log('Aun no has pagado');
         break;
 }
