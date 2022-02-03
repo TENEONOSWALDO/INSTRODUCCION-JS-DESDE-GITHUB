1d6214ee9f1904342036db89ab40a4169918e833
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-02 20:27:41 -0500

    Como utilizar el while loop

--- js/24.js
+++ js/24.js
@@ -1,4 +1,3 @@
-
 // For loop
 
 console.log(1);
@@ -10,32 +9,41 @@ console.log(4);
 //   console.log(i);
 //}
 
-for ( let i = 1; i<=100 ; i++) {
-    if ( i % 2 ===0 ) {
-        console.log(`El Número ${i} es PAR`);
-    }else {
-        console.log(`El Número ${i} es IMPAR`);
-    }
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(`El Número ${i} es PAR`);
+  } else {
+    console.log(`El Número ${i} es IMPAR`);
+  }
 }
 
-
 const carrito = [
-    { nombre : 'Monitor 20 Pulgadas', precio: 500 },
-    { nombre : 'Television 50 Pulgadas', precio: 700 },
-    { nombre : 'Tablet', precio: 300 },
-    { nombre : 'Audifonos', precio: 200 },
-    { nombre : 'Teclado', precio: 50 },
-    { nombre : 'Celular', precio:  500 },
-    { nombre : 'Bocinas', precio: 300 },
-    { nombre : 'Laptop', precio: 800 },
-    ];
+  { nombre: "Monitor 20 Pulgadas", precio: 500 },
+  { nombre: "Television 50 Pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audifonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 }
+];
 
-    for(let i = 0 ; i < carrito.length; i++) {
-            console.log( carrito [i]. nombre) ;
-    }
+for (let i = 0; i < carrito.length; i++) {
+  console.log(carrito[i].nombre);
+}
 
+// While Loop Loop Se ejecuta mientras una condicion sea evaluada como verdadera.
 
-    // While Loop Loop Se ejecuta mientras una condicion sea evaluada como verdadera.
+//let i = 0; // indice
+//while (i < 10) {  // Condicion
+//console.log(i)
 
+ //   i++; // incremento
+//}
 
-let i = o ;
+let i = 1 ;
+while( i <= 100) { // Condicion
+ if ( i % 2 === 0)
+ console.log( `El numero ${i} es par`)
+ i++ ;
+}
