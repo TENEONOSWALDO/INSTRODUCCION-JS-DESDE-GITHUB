b7b7e8489928c8b4220febaf1614a9ad93d7bb42
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-05 17:44:44 -0500

    como se usa this?

--- js/26.js
+++ js/26.js
@@ -11,4 +11,17 @@ const reservacion ={
         console.log(`El Cliente ${reservacion.nombre} reservó y su cantidad a pagar es de ${reservacion.total}`);
     }
 }
-console.log (reservacion.informacion());
+
+
+
+const reservacion2 ={
+    nombre: 'Pedro',
+    apellido: 'De la torre',
+    total: 5000,
+    pagado: false,
+    informacion: function() {
+        console.log(`El Cliente ${this.nombre} reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+reservacion.informacion();
+reservacion2.informacion();
