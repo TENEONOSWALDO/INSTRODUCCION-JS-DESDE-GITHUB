8fd443113e774865f885c936a754c1943cc8dd34
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-05 18:41:10 -0500

    Crear file 27.js y comentar sobre Objecto,literal y object constructor

---
+++ js/27.js
@@ -0,0 +1,21 @@
+
+// Programacion Orientada a Objetos(POO)
+// Object Literal
+const producto = {
+    nombre: 'table',
+    precio: 500,
+}
+
+//Object Constructor
+
+function Producto(nombre, precio){
+        this.nombre=nombre;
+        this.precio=precio;
+}
+
+const producto2 = new Producto('Monitor Curvo de 49"', 800);
+const producto3 = new Producto('Laptop', 300);
+
+
+console.log(producto2);
+console.log(producto3);
