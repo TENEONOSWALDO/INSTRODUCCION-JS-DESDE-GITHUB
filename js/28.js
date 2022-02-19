1d585ed07905b6022d306faeeb4435345255209e
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-19 17:51:39 -0500

    Clases en js

--- js/28.js
+++ js/28.js
@@ -1,1 +1,15 @@
 
+// Clases 
+
+class Producto  {
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const producto2= new Producto('Monitor Curvo de 49"', 800);
+const producto3 = new Producto('Laptop', 300);
+
+console.log(producto2);
+console.log(producto3);
