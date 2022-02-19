292212a3faa7c3cf92f51a1606273b1a1570cb79
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-19 15:23:44 -0500

    crear funciones que solo se utilizan en un objeto en especifico

--- js/27.js
+++ js/27.js
@@ -13,9 +13,15 @@ function Producto(nombre, precio){
         this.precio=precio;
 }
 
+
 const producto2 = new Producto('Monitor Curvo de 49plg ', 800);
 const producto3 = new Producto('Laptop',300);
 
-
+function formatearProducto(producto){
+    return `El producto ${producto.nombre} tiene un precio de: $  ${producto.precio }`;
+} 
 console.log(producto2);
 console.log(producto3);
+
+
+console.log ( formatearProducto(producto2));
