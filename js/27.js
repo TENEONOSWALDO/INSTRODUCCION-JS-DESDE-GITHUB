3294437a9b9e3e2877a2cea10e9829128c252ad7
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-19 17:32:41 -0500

    Crear 28.js

--- js/27.js
+++ js/27.js
@@ -16,12 +16,19 @@ function Producto(nombre, precio){
 
 const producto2 = new Producto('Monitor Curvo de 49plg ', 800);
 const producto3 = new Producto('Laptop',300);
+// CREAR FUNCIONES QUE SOLO SE UTILIZAN EN UN OBJETO EN ESPECIFICO.
+
+
+Producto.prototype.formatearProducto = function() {
+    return `El Producto ${this.nombre} tiene un precio de: $  ${producto.precio }`;
+} 
+const cliente = new cliente ('Juan', 'De la torre');
+
+console.log(cliente);
 
 function formatearProducto(producto){
     return `El producto ${producto.nombre} tiene un precio de: $  ${producto.precio }`;
 } 
-console.log(producto2);
-console.log(producto3);
 
-
-console.log ( formatearProducto(producto2));
+console.log(producto2.formatearProducto());
+console.log(producto3.formatearProducto()) ;
