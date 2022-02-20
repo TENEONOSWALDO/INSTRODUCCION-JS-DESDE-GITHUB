c15af5fbc8b9033dc654b959938d46c3ac632fd1
TENEONOSWALDO <teneonoswaldo@hotmail.com>
2022-02-19 20:11:11 -0500

    Ejemplo de clase

--- js/28.js
+++ js/28.js
@@ -1,15 +1,18 @@
 
-// Clases 
+// CLASES
 
-class Producto  {
-    constructor(nombre, precio){
-        this.nombre = nombre;
-        this.precio = precio;
+class Producto {
+    constructor (nombre, precio ) {
+        this.nombre = nombre ;
+        this.precio = precio ;
+    }
+    formatearProducto(){
+        return  `El Producto ${this.nombre} tiene un precio de : $ ${this.precio}`;
     }
 }
 
-const producto2= new Producto('Monitor Curvo de 49"', 800);
-const producto3 = new Producto('Laptop', 300);
+const producto2 = new Producto ('Monitor  curvo de 49"', 800) ;
+const producto3 = new Producto ('Laptop',300);
 
 console.log(producto2);
 console.log(producto3);
